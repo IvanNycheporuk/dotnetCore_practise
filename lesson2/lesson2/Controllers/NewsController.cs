f6e3dcee1a3486b14ab8826c3e80f6bb2492a66e
ivan_nycheporuk <ivan.nycheporuk@gmail.com>
2021-03-21 21:19:03 +0200

    completed hw2 | task4 clarification is needed

---
+++ lesson2/lesson2/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using lesson2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lesson2.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["News"] = NewsBase.GetNews();
+            return View();
+        }
+    }
+}
